8722d6d8ccf54b8b8543a4260325c4d574ecc898
Bob Myers <rtm@gol.com>
2021-04-04 18:56:02 -0700

    Retrieve config input

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const fs = require("fs").promises;
 
 try {
   // `who-to-greet` input defined in action metadata file
+  const config = core.getInput("config");
+  console.log("config is", config);
+
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // console.log(`The event payload: ${payload}`);
 
   (async () => {
     // Creat a directory and a file within it.
