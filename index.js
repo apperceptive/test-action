20f84c3627456612bc3e2e085649d8ea02b2d91c
Bob Myers <rtm@gol.com>
2021-04-04 18:24:53 -0700

    Add callback to mkdir

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const artifact = require("@actions/artifact");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -13,11 +13,11 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
-  // Creat a directory and a file within it.
-  fs.mkdir("foobar");
-  fs.writeFileSync("foobar/batshit", "test file");
-
   (async () => {
+    // Creat a directory and a file within it.
+    await fs.mkdir("foobar");
+    await fs.writeFileSync("foobar/batshit", "test file");
+
     const uploadResponse = await artifact.uploadArtifact("my-artifact", ["foobar/batshit"], {});
   })();
 } catch (error) {
